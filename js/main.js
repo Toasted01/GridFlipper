0bd8c305143aa9f7bed49b3184b2cd93fa466ae7
Tom Burns <tomb7030@gmail.com>
2023-07-22 17:59:55 +0100

    Gaming update
    Added a win condition upon making all squares white.
    Also added the ability to re-roll the grid to make a random starting square black.
    Prettified the code to make website look nicer and compartmentalise code.
    Left in the 1 click win at the start for proof/testing purposes.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,8 @@
-const btnClick = (btnId) => {
-    let btnArray = getAdjacent(btnId);
-    console.log(btnArray);
-
-    btnArray.forEach(element => {
-        let thisBtn = window.getComputedStyle(document.getElementById(element)).backgroundColor;
-        (thisBtn == "rgb(0, 0, 0)") ? isBlack(element) : isWhite(element);
-    });
-
-    //let thisBtn = window.getComputedStyle(document.getElementById(btnId)).backgroundColor;
-    //(thisBtn == "rgb(0, 0, 0)") ? isBlack(btnId) : isWhite(btnId);
-    
-}
-
 const isBlack = (btnId) => {
-    console.log("Black", btnId, typeof btnColor);
     document.getElementById(btnId).style.backgroundColor = "white";
 }
 
 const isWhite = (btnId) => {
-    console.log("White", btnId, typeof btnColor);
     document.getElementById(btnId).style.backgroundColor = "Black";
 }
 
@@ -39,17 +23,74 @@ const getAdjacent = (btnId) => {
             }
         }
     }
-    console.log(adjacent);
     return adjacent;
+}
+
+const getXY = (x,y) =>{
+    let stringX = x.toString();
+    let stringY = y.toString();            
+    let xy = stringX.concat(stringY);
+    return xy;
+
 }
 
 const checkBounds = (x,y) =>{  
       if(x>0 && x<6){
         if(y>0 && y<6){
-            let stringX = x.toString();
-            let stringY = y.toString();            
-            let xy = stringX.concat(stringY);
+            let xy = getXY(x,y);
             return xy;
         }
     }                    
+}
+
+const winCondition = async () =>{
+    let count = 0;
+    for(let x = 1; x <= 5; x++){
+        for(let y=1;y<=5;y++){
+            let xy = getXY(x,y);
+            if( window.getComputedStyle(document.getElementById(xy)).backgroundColor == "rgb(255, 255, 255)")
+            {
+                count++;
+            }
+        }
+    }
+    if(count==25){
+        document.getElementById("winner").innerHTML = "!Winner!";
+        for(let x = 1; x <= 5; x++){
+            for(let y=1;y<=5;y++){
+                let xy = getXY(x,y);
+                document.getElementById(xy).setAttribute("disabled","disabled");
+            }
+        }
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+const startBtn = () =>{    
+    for(let x = 1; x <= 5; x++){
+        for(let y=1;y<=5;y++){
+            let xy = getXY(x,y);
+            document.getElementById(xy).removeAttribute("disabled");
+            document.getElementById(xy).style.backgroundColor = "white";
+            document.getElementById("winner").innerHTML = "Click on a square to invert it's color and the adjacent squares too.";
+        }
+    }
+    let x = Math.round(getRandomArbitrary(1,5));
+    let y = Math.round(getRandomArbitrary(1,5));
+    let blackXY = getXY(x,y);
+    console.log(blackXY);
+    document.getElementById(blackXY).style.backgroundColor = "black";
+}
+
+const flip = (btnId) => {
+    let btnArray = getAdjacent(btnId);
+    btnArray.forEach(element => {
+        let thisBtn = window.getComputedStyle(document.getElementById(element)).backgroundColor;
+        (thisBtn == "rgb(0, 0, 0)") ? isBlack(element) : isWhite(element);
+    }); 
+    console.log("here");
+    winCondition();
 }
