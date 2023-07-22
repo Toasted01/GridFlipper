2becd4933812a42c06daf2e59ad28589cc88c501
Tom Burns <tomb7030@gmail.com>
2023-07-22 18:42:28 +0100

    Final Update
    Disabled all buttons on startup.
    Removed unnecessary async from winCondition.
    Allowed for up to 5 random starting squares at a time.
    Added comments to js.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
+/**
+ * function to turn black buttons white
+ * @param {*} btnId 
+ */
 const isBlack = (btnId) => {
     document.getElementById(btnId).style.backgroundColor = "white";
 }
 
+/**
+ * function to turn white buttons black
+ * @param {*} btnId 
+ */
 const isWhite = (btnId) => {
     document.getElementById(btnId).style.backgroundColor = "Black";
 }
 
+/**
+ * gets all the adjacent squares as well as the clicked square and puts them in an array to return
+ * @param {*} btnId 
+ * @returns 
+ */
 const getAdjacent = (btnId) => {
     let idInt = parseInt(btnId);
     let btnY = idInt%10;
@@ -26,6 +39,12 @@ const getAdjacent = (btnId) => {
     return adjacent;
 }
 
+/**
+ * turns int x,y co ordinates into xy string
+ * @param {*} x 
+ * @param {*} y 
+ * @returns 
+ */
 const getXY = (x,y) =>{
     let stringX = x.toString();
     let stringY = y.toString();            
@@ -34,6 +53,12 @@ const getXY = (x,y) =>{
 
 }
 
+/**
+ * Checks to see if x,y value are within the boards range
+ * @param {*} x 
+ * @param {*} y 
+ * @returns 
+ */
 const checkBounds = (x,y) =>{  
       if(x>0 && x<6){
         if(y>0 && y<6){
@@ -43,7 +68,11 @@ const checkBounds = (x,y) =>{
     }                    
 }
 
-const winCondition = async () =>{
+/**
+ * Check to see if all 25 squares are white
+ * If win condition met disable the board
+ */
+const winCondition =() =>{
     let count = 0;
     for(let x = 1; x <= 5; x++){
         for(let y=1;y<=5;y++){
@@ -65,11 +94,21 @@ const winCondition = async () =>{
     }
 }
 
+/**
+ * Get a pure random number between 2 numbers inclusive
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
-  }
+}
 
+/**
+ * onClick button that wipes the board clean then produces between 1-5 random black squares on the board
+ */
 const startBtn = () =>{    
+    //wiping the board
     for(let x = 1; x <= 5; x++){
         for(let y=1;y<=5;y++){
             let xy = getXY(x,y);
@@ -78,13 +117,35 @@ const startBtn = () =>{
             document.getElementById("winner").innerHTML = "Click on a square to invert it's color and the adjacent squares too.";
         }
     }
-    let x = Math.round(getRandomArbitrary(1,5));
-    let y = Math.round(getRandomArbitrary(1,5));
-    let blackXY = getXY(x,y);
-    console.log(blackXY);
-    document.getElementById(blackXY).style.backgroundColor = "black";
+
+    //asigning black squares without dupes
+    let count=0;
+    let usedSquare = [];    
+    usedSquare.push("00");
+    while(count<Math.round(getRandomArbitrary(1,5))){
+        let x = Math.round(getRandomArbitrary(1,5));
+        let y = Math.round(getRandomArbitrary(1,5));
+        let blackXY = getXY(x,y);
+        let dupe = false;
+        
+        usedSquare.forEach(element => {
+            if(blackXY==element){
+                dupe = true;
+            }            
+        })
+
+        if(dupe==false){
+            document.getElementById(blackXY).style.backgroundColor = "black";
+            usedSquare.push(blackXY);
+            count++;            
+        }
+    }
 }
 
+/**
+ * function to flip color of square and adjacent squares then check win condition
+ * @param {*} btnId 
+ */
 const flip = (btnId) => {
     let btnArray = getAdjacent(btnId);
     btnArray.forEach(element => {
